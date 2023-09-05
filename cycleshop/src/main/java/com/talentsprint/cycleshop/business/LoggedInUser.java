7d2d2c38d77505da3e18830f7ebabc477cf3ab6c
ankan.m <ankan.m@prodapt.com>
2023-09-05 20:39:54 +0530

    Cycle Shop with user added

---
+++ cycleshop/src/main/java/com/talentsprint/cycleshop/business/LoggedInUser.java
@@ -0,0 +1,10 @@
+package com.talentsprint.cycleshop.business;
+
+import com.talentsprint.cycleshop.entity.User;
+
+import lombok.Data;
+
+@Data
+public class LoggedInUser {
+    private User loggedInUser;
+}
