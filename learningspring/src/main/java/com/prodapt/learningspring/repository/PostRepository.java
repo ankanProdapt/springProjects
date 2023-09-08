9b91e5222f5f9baea0e65f64bb3eb909b8ed0d1f
ankan.m <ankan.m@prodapt.com>
2023-09-08 14:49:13 +0530

    Added few queries in PostRepository

--- learningspring/src/main/java/com/prodapt/learningspring/repository/PostRepository.java
+++ learningspring/src/main/java/com/prodapt/learningspring/repository/PostRepository.java
@@ -1,9 +1,14 @@
 package com.prodapt.learningspring.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.prodapt.learningspring.entity.Post;
 
 public interface PostRepository extends CrudRepository<Post, Integer>{
+    @Query(value = "SELECT COUNT(*) FROM post WHERE post.author_id = ?1 AND DATE(post.created_at) = DATE(NOW())", nativeQuery = true)
+    int CountPostsCreatedTodayBy(int id);
 
+    @Query(value = "SELECT COUNT(*) FROM post WHERE post.author_id = ?1 AND MONTH(post.created_at) = MONTH(NOW())", nativeQuery = true)
+    int CountPostsCreatedInThisMonthBy(int id);
 }
